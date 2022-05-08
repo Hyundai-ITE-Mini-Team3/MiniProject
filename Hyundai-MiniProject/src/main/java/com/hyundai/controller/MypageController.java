2de092c5b9f011398c84e61bce5b6a125b7bc7dc
0suh <yschin0508@gmail.com>
2022-05-08 14:09:28 +0900

    마이페이지, QnA, 댓글 컨트롤러 주석

--- Hyundai-MiniProject/src/main/java/com/hyundai/controller/MypageController.java
+++ Hyundai-MiniProject/src/main/java/com/hyundai/controller/MypageController.java
@@ -19,7 +19,13 @@ import com.hyundai.service.OrderService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+/*************************************************************
+파일명: MypageController.java
+기능: 마이 페이지 이동
+작성자: 진영서
 
+[코멘트: 로그인 상태에서 마이페이지로 이동, 주문 내역 확인가능]
+*************************************************************/
 @Controller
 @RequestMapping("/mypage/*")
 @Log4j
@@ -29,25 +35,37 @@ public class MypageController {
 	@Autowired
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	// OrderService 객체 주입
 	@Autowired
 	private OrderService o_service;
 	
+	// mypage.jsp로 이동
 	@RequestMapping(value = "/mypage", method = RequestMethod.GET)
 	public String mypage(HttpServletRequest request, Model model) {
 		logger.info("=============mypage=============");
-		HttpSession session = request.getSession();
-		String mid = (String)session.getAttribute("member_id");
-		String mname = (String)session.getAttribute("member_name");
+		// 세션에서 값 조회
+		HttpSession session = request.getSession(); 
+		String mid = (String)session.getAttribute("member_id"); //세션에 저장된 로그인 유저 아이디 저장
+		String mname = (String)session.getAttribute("member_name"); //세션에 저장된 로그인 유저 이름 저장
 		logger.info("Session(mid): " + mid);
 		logger.info("Session(mname): " + mname);
-		MemberVO user = new MemberVO();
-		user.setMid(mid);
-		user.setMname(mname);
-		model.addAttribute("user", user);
 		
-		List<OrderVO> orderList = o_service.getOrder(mid);
-		model.addAttribute("orders", orderList);
-		
-		return "/mypage/mypage";
+		if (mid != null) {	// 로그인 정보 있으면
+			// MemberVO 객체 생성 후 값 설정(mid, mname)
+			MemberVO user = new MemberVO(); 
+			user.setMid(mid);
+			user.setMname(mname);
+			model.addAttribute("user", user);
+			
+			// OrderService의 getOrder() 호출
+			//해당 아이디의 주문 내역을 불러와 리스트에 저장
+			List<OrderVO> orderList = o_service.getOrder(mid);
+			model.addAttribute("orders", orderList);
+			return "/mypage/mypage";
+
+		} else {	// 로그인 정보 없으면 로그인 페이지로 이동
+
+			return "/login/loginform";
+		}
 	}
 }
--- Hyundai-MiniProject/src/main/java/com/hyundai/controller/ReplyController.java
+++ Hyundai-MiniProject/src/main/java/com/hyundai/controller/ReplyController.java
@@ -21,7 +21,13 @@ import com.hyundai.service.ReplyService;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j;
+/*************************************************************
+파일명: ReplyController.java
+기능: 댓글 작성/조회/수정/삭제
+작성자: 진영서
 
+[코멘트: Ajax 통신을 통해 댓글 구현, 로그인한 유저와 작성자가 일치할 경우만 댓글 수정/삭제 가능]
+*************************************************************/
 @RequestMapping("/replies")
 @RestController
 @Log4j
@@ -29,71 +35,92 @@ import lombok.extern.log4j.Log4j;
 
 public class ReplyController {
 	@Autowired
+	// Service 객체 주입
 	private ReplyService service;
 	
+	//게시물 당 댓글 조회
+	// Parameter: qid(글번호)
 	@GetMapping(value = "/qid/{qid}",
 			produces = {MediaType.APPLICATION_XML_VALUE,
 					MediaType.APPLICATION_JSON_UTF8_VALUE}	)
 	public ResponseEntity<List<ReplyVO>> getList(@PathVariable("qid") Long qid ){
 		
 		log.info("getList..........");
-		return new ResponseEntity<>(service.getList(qid), HttpStatus.OK);
+		//ReplyService의 getList() 호출
+		return new ResponseEntity<>(service.getList(qid), HttpStatus.OK); //댓글 리스트 리턴
 		
-	}//end getList
+	}
 
+	//댓글 작성
 	@PostMapping(value = "/new",
 			consumes = "application/json" //요청 줄이기
 			,produces = {MediaType.TEXT_PLAIN_VALUE})
 	public ResponseEntity<String> create(@RequestBody ReplyVO vo){
 		
 		log.info("ReplyVO" + vo);	
-		int insertCount = service.register(vo);
 		
+		//ReplyService의 register() 호출
+		int insertCount = service.register(vo); //댓글 insert 성공시 1 반환
 		log.info("Reply INSERT COUNT : " + insertCount);
 		
 		//삼항식 conditional operator
+		//댓글 작성 성공시 success 리턴
 		return ( insertCount == 1
 				? new ResponseEntity<>("success",HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)
 		);		
-	}//end create
+	}
 	
+	
+	//댓글 조회
 	@GetMapping(value = "/{rid}",
 			produces = {MediaType.APPLICATION_XML_VALUE,
 					MediaType.APPLICATION_JSON_UTF8_VALUE})
 	public ResponseEntity<ReplyVO> get(
 			@PathVariable("rid") Long rid ){		
 		log.info("get" + rid);		
-		return new ResponseEntity<>(service.get(rid),HttpStatus.OK );
+		return new ResponseEntity<>(service.get(rid),HttpStatus.OK ); //댓글 번호를 통해 댓글 객체 정보 리턴
 	}//end get
 	
+	
+	//댓글 삭제
+	// Parameter: rid(댓글번호)
 	@DeleteMapping(value = "/{rid}",produces = {MediaType.TEXT_PLAIN_VALUE})
 	public ResponseEntity<String> remove(
 			@PathVariable("rid") Long rid	){		
 		log.info("remove" + rid);		
-		//삼항식
+
+		//ReplyService의 remove() 호출
+		//댓글 삭제 성공시 success 리턴
 		return (service.remove(rid) == 1
 				? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR)
 				);		
-	}//end remove	
+	}
 	
+	
+	//댓글 수정
+	// Parameter: rid(댓글번호)
 	@RequestMapping(value = "/{rid}",
 			method = {RequestMethod.PUT, RequestMethod.PATCH},
 			consumes = "application/json",
 			produces = {MediaType.TEXT_PLAIN_VALUE} 	)
 	public ResponseEntity<String> modify(
 			@RequestBody ReplyVO vo,
-			@PathVariable("rid") Long rid	){		
+			@PathVariable("rid") Long rid	){	
+		
+		//수정하는 댓글의 번호 저장
 		vo.setRid(rid);
 		log.info("rid" + rid);
-		log.info("modify" + vo);		
+		log.info("modify" + vo); //수정될 댓글객체
+		
+		//ReplyService의 modify() 호출
+		//댓글 수정 성공시 success 리턴
 		return (service.modify(vo) == 1
 				? new ResponseEntity<>("success", HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR) 
-				);//end return		
-	}//end modify
-
+				);	
+	}
 
 
 }
